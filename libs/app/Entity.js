de00b37073af74be1a7446355f9f0d3815f3e302
Tibi Georgescu <tiberiu.georgescu@mavenhut.com>
2015-07-16 15:31:04 +0000

    [WIP] More enitty improvements

--- libs/app/Entity.js
+++ libs/app/Entity.js
@@ -18,12 +18,14 @@
 		},
 			console,
 			registry,
+			DOM,
 			EventInstance,
 			Event;
 		
 		// Dependencies	
 		console = I.instance('tools.console');
 		registry = I.require('core.registry');
+		DOM = I.require('libs.html.Selector');
 		EventInstance = I.require('libs.events.Dispatcher');
 		Event = new EventInstance('Entity');
 		
@@ -32,22 +34,36 @@
 		 */
 		Entity.instance = function (name) {
 			this.name = name;
+			
+			Event.dispatch({
+				namespace: 'App',
+				type: 'action',
+				action: 'entity-register'
+			}, this.name);
 		};
 		
 		/**
 		 * Define an action fot entity
 		 */
 		Entity.instance.prototype.action = function (header, body) {
-			var entity = {};
+			var entity = {},
+				context;
 			
-			entity.setValueOf('header', this.name + '.' + header);
-			entity.setValueOf('body', body);
-			entity.setValueOf('type', 'action');
+			context = {
+				name: this.name,
+				header: header,
+				type: 'action'	
+			};
+			
+			context.setValueOf('DOM', DOM);
+			
+			entity = Object.copy(context);
+			entity.setValueOf('body', body.bind(context));
 			
 			Event.dispatch({
 				namespace: 'App',
 				type: 'action',
-				action: 'entity-register'
+				action: 'entity-update'
 			}, entity);
 		};
 		
@@ -55,21 +71,29 @@
 		 * Define an event fot entity
 		 */
 		Entity.instance.prototype.event = function (header, body) {
-			var entity = {};
+			var entity = {},
+				context;
 			
-			entity.setValueOf('header', this.name + '.' + header);
-			entity.setValueOf('body', body);
-			entity.setValueOf('type', 'action');
+			context = {
+				name: this.name,
+				header: header,
+				type: 'event'	
+			};
+			
+			context.setValueOf('DOM', DOM);
+			
+			entity = Object.copy(context);
+			entity.setValueOf('body', body.bind(context));
 			
 			Event.dispatch({
 				namespace: 'App',
 				type: 'action',
-				action: 'entity-register'
+				action: 'entity-update'
 			}, entity);
 		};
 		
 		/**
-		 * Register component
+		 * Register Entity component
 		 */
 		this.exposer.register({
 			namespace: 'entity',
